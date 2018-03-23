c4a4cc9409a963164a71de4363d83e43e1979f6e
Artem <1308-87@mail.ru>
2018-03-23 15:01:21 +0000

    commit

--- app.js
+++ app.js
@@ -1,44 +1,55 @@
-var express = require("express");
-var app = express();
-var bodyParser = require("body-parser")
+var express     = require("express"),
+    app         = express(),
+    bodyParser  = require("body-parser"),
+    mongoose    = require("mongoose");
 
+mongoose.connect("mongodb://localhost/yelp_camp");
 app.use(bodyParser.urlencoded({extended:true}));
 app.set("view engine","ejs");
- var campgrounds = [
-        {name: "Salmon Creek", image: "https://farm9.staticflickr.com/8442/7962474612_bf2baf67c0.jpg"},
-        {name: "Granite Hill", image: "https://farm1.staticflickr.com/60/215827008_6489cd30c3.jpg"},
-        {name: "Mountain Goat's Rest", image: "https://farm7.staticflickr.com/6057/6234565071_4d20668bbd.jpg"},
-        {name: "Salmon Creek", image: "https://farm9.staticflickr.com/8442/7962474612_bf2baf67c0.jpg"},
-        {name: "Granite Hill", image: "https://farm1.staticflickr.com/60/215827008_6489cd30c3.jpg"},
-        {name: "Mountain Goat's Rest", image: "https://farm7.staticflickr.com/6057/6234565071_4d20668bbd.jpg"},
-        ];
+
+//schema setup
+var campgroundSchema = new mongoose.Schema({
+    name:String,
+    image:String
+});
+
+var Campground = mongoose.model("Campground", campgroundSchema);
 
 app.get("/",function(req,res){
-    res.render("landing")
+    res.render("landing");
 });
 
 app.get("/campgrounds",function(req,res){
-    
-   
-    res.render("campgrounds",{campgrounds:campgrounds})
-});
+    //get all components from DB
+    Campground.find({},function(err,allCampgrounds){
+        if(err){
+            console.log(err);
+        }else{
+            res.render("campgrounds",{campgrounds:allCampgrounds})
+            }
+        });
+    });
+    // res.render("campgrounds",{campgrounds:campgrounds})
 
 app.post("/campgrounds",function(req,res){
     //get data from form and add to the campgrounds array
    var name= req.body.name;
    var image =req.body.image;
-   var newCampground = {name: name, image: image}
-   campgrounds.push(newCampground);
-    //redirect to campground page
-    res.redirect("/campgrounds");
+   var newCampground = {name: name, image: image};
+   //create new campground and save to DB
+   Campground.create(newCampground,function(err,newlyCreated){
+       if(err){
+           console.log(err);
+       }else{
+           //redirect to campground page
+           res.redirect("/campgrounds");
+       }
+   });
 });
 
 app.get("/campgrounds/new",function(req,res){
-    res.render("new")
+    res.render("new");
 });
-
-
-
 
 app.listen(process.env.PORT,process.env.IP, function(){
     console.log("the yelpcamp server has started");
