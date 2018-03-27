7bc3e7a9b201b92ebfc940be5f24bb8dea3fbfa1
Artem <1308-87@mail.ru>
2018-03-27 13:46:56 +0000

    commit

--- app.js
+++ app.js
@@ -10,7 +10,8 @@ app.set("view engine","ejs");
 //schema setup
 var campgroundSchema = new mongoose.Schema({
     name:String,
-    image:String
+    image:String,
+    description:String
 });
 
 var Campground = mongoose.model("Campground", campgroundSchema);
@@ -25,7 +26,7 @@ app.get("/campgrounds",function(req,res){
         if(err){
             console.log(err);
         }else{
-            res.render("campgrounds",{campgrounds:allCampgrounds})
+            res.render("index",{campgrounds:allCampgrounds})
             }
         });
     });
@@ -35,7 +36,8 @@ app.post("/campgrounds",function(req,res){
     //get data from form and add to the campgrounds array
    var name= req.body.name;
    var image =req.body.image;
-   var newCampground = {name: name, image: image};
+   var desc =req.body.description;
+   var newCampground = {name: name, image: image,description:desc};
    //create new campground and save to DB
    Campground.create(newCampground,function(err,newlyCreated){
        if(err){
@@ -50,7 +52,18 @@ app.post("/campgrounds",function(req,res){
 app.get("/campgrounds/new",function(req,res){
     res.render("new");
 });
-
+//SHOW - shows more info about campground
+app.get("/campgrounds/:id",function(req,res){
+    //find the campground with provided id
+    Campground.findById(req.params.id,function(err,foundCampground){
+        if(err){
+            console.log(err);
+        }else{
+            //render show template
+            res.render("show",{campground:foundCampground});
+        }
+    })
+})
 app.listen(process.env.PORT,process.env.IP, function(){
     console.log("the yelpcamp server has started");
 });
